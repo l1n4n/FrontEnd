db858dabd2036411e122d0b19749fd303624f7ee
l1n4n <35434784+l1n4n@users.noreply.github.com>
2018-04-27 16:10:06 -0400

    Add files via upload

---
+++ HW5/AddItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class AddItem extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      newItem:{}
+    }
+  }
+
+  handleSubmit(e) {
+      e.preventDefault(); // this prevents the page from reloading -- do not delete this line!
+
+      // Implement the rest of this function here!
+      var name = this.props.idName;
+      var data = this.refs.id.value;
+      this.setState({
+          name : name,
+          newItem : {name : this.refs.id.value}
+      },
+       () => this.props.addItem(this.state));    // passing props must be within setState 
+}
+    
+
+  render() {
+    var divName = 'add' + this.props.idName;
+    return (
+      <div className='addItemDiv'>
+      <h4>Add {this.props.idName}</h4>
+      <form ref='form' onSubmit={this.handleSubmit.bind(this)}>
+      <div id={divName} ref={divName}>
+        <label>Name</label><br />
+        <input type='text' ref='id' />
+        </div>
+        <br />
+        <input type='submit' value='Submit' />
+        <br />
+      </form>
+      </div>
+    );
+  }
+
+}
+
+export default AddItem;
